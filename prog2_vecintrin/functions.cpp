679ee54999dc7d9667159c4f4f9f689ef7271b8e
Arjun Walia <walia.arjun.2004@gmail.com>
2025-09-10 16:06:59 -0400

    Problem 2

--- prog2_vecintrin/functions.cpp
+++ prog2_vecintrin/functions.cpp
@@ -83,6 +83,57 @@ void clampedExpSerial(float* values, int* exponents, float* output, int N) {
 void clampedExpVector(float* values, int* exponents, float* output, int N) {
     // Implement your vectorized version of clampedExpSerial here
     //  ...
+
+    __cmu418_vec_float x;
+    __cmu418_vec_int y;
+    __cmu418_vec_float result;
+    __cmu418_vec_float xpower;
+    __cmu418_vec_float ones = _cmu418_vset_float(1.f);
+    __cmu418_vec_float clamp_val = _cmu418_vset_float(4.18f);
+    __cmu418_vec_int int_ones = _cmu418_vset_int(1);
+    __cmu418_vec_int zeros = _cmu418_vset_int(0);
+    __cmu418_mask maskAll, maskActive, maskOddBit, maskGreaterThanClamp;
+
+    for (int i = 0; i < N; i += VECTOR_WIDTH) {
+        int remaining = N - i;
+        if (remaining >= VECTOR_WIDTH) {
+            maskAll = _cmu418_init_ones();
+        } else {
+            maskAll = _cmu418_init_ones(remaining);
+        }
+        
+        _cmu418_vload_float(x, values + i, maskAll);
+        _cmu418_vload_int(y, exponents + i, maskAll);
+        
+        result = _cmu418_vset_float(1.f);
+        xpower = x;
+        
+        _cmu418_vgt_int(maskActive, y, zeros, maskAll);
+        
+        while (_cmu418_cntbits(maskActive) > 0) {
+            maskOddBit = _cmu418_init_ones(0);
+            
+            __cmu418_vec_int temp_and;
+            _cmu418_vbitand_int(temp_and, y, int_ones, maskAll);
+            _cmu418_vgt_int(maskOddBit, temp_and, zeros, maskAll);
+            
+            maskOddBit = _cmu418_mask_and(maskOddBit, maskActive);
+            
+            _cmu418_vmult_float(result, result, xpower, maskOddBit);
+            
+            _cmu418_vmult_float(xpower, xpower, xpower, maskActive);
+            
+            _cmu418_vshiftright_int(y, y, int_ones, maskActive);
+            
+            _cmu418_vgt_int(maskActive, y, zeros, maskAll);
+        }
+        
+        _cmu418_vgt_float(maskGreaterThanClamp, result, clamp_val, maskAll);
+        _cmu418_vset_float(result, 4.18f, maskGreaterThanClamp);
+        
+        _cmu418_vstore_float(output + i, result, maskAll);
+    }
+
 }
 
 
@@ -98,7 +149,21 @@ float arraySumSerial(float* values, int N) {
 // Assume N % VECTOR_WIDTH == 0
 // Assume VECTOR_WIDTH is a power of 2
 float arraySumVector(float* values, int N) {
-    // Implement your vectorized version here
+	// Implement your vectorized version here
     //  ...
-	return 0.f;
+    __cmu418_vec_float vec_sum = _cmu418_vset_float(0.f);
+    __cmu418_vec_float vec_values;
+    __cmu418_mask maskAll = _cmu418_init_ones();
+    
+    for (int i = 0; i < N; i += VECTOR_WIDTH) {
+        _cmu418_vload_float(vec_values, values + i, maskAll);
+        _cmu418_vadd_float(vec_sum, vec_sum, vec_values, maskAll);
+    }
+    
+    float result = 0.f;
+    for (int i = 0; i < VECTOR_WIDTH; i++) {
+        result += vec_sum.value[i];
+    }
+    
+    return result;
 }
